2fe34f9db22ed46d84b4edc5b3bb2447112d88a9
do_muhammad.amin <muhammad.amin@digitalomega.com>
2024-10-03 17:20:58 +0500

    Added error and global error pages

--- next.config.js
+++ next.config.js
@@ -2,28 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     productionBrowserSourceMaps: true,  // Enable source maps in production
-    async headers() {
-        return [
-          {
-            // matching all API routes
-            source: "/trpc/:path*",
-            headers: [
-              { key: "Access-Control-Allow-Credentials", value: "true" },
-              { key: "Access-Control-Allow-Origin", value: "*" },
-              { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-              { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-            ]
-          }
-        ]
-      },
-      async rewrites() {
-        return [
-          {
-            source: '/trpc/:path*',
-            destination: '/api/trpc/:path*',
-          },
-        ];
-      },
+    
 images:{
     remotePatterns:[
         {
